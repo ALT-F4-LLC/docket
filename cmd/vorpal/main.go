4128a4c23d2e422a7f9d48b9c43e2fa7c9c85fe7
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2026-07-14 14:05:09 -0700

    chore(vorpal): update configuration with latest sdk improvements

--- cmd/vorpal/main.go
+++ cmd/vorpal/main.go
@@ -4,89 +4,84 @@ import (
 	"fmt"
 	"log"
 
-	api "github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/api/artifact"
 	"github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/artifact"
 	"github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/artifact/language"
 	"github.com/ALT-F4-LLC/vorpal/sdk/go/pkg/config"
 )
 
-var Systems = []api.ArtifactSystem{
-	api.ArtifactSystem_AARCH64_DARWIN,
-	api.ArtifactSystem_AARCH64_LINUX,
-	api.ArtifactSystem_X8664_DARWIN,
-	api.ArtifactSystem_X8664_LINUX,
-}
-
 func main() {
-	context := config.GetContext()
-	contextTarget := context.GetTarget()
+	ctx := config.GetContext()
+	ctxTarget := ctx.GetTarget()
 
-	// Artifact dependencies
+	systems := []string{
+		"aarch64-darwin",
+		"aarch64-linux",
+		"x86_64-darwin",
+		"x86_64-linux",
+	}
 
-	ffmpeg, err := context.FetchArtifactAlias("ffmpeg:8.0.1")
+	ffmpeg, err := ctx.FetchArtifactAlias("ffmpeg:8.0.1")
 	if err != nil {
 		log.Fatalf("failed to get ffmpeg: %v", err)
 	}
 
-	gobin, err := artifact.GoBin(context)
+	gobin, err := artifact.GoBin(ctx)
 	if err != nil {
 		log.Fatalf("failed to get go: %v", err)
 	}
 
-	goimports, err := artifact.Goimports(context)
+	goimports, err := artifact.Goimports(ctx)
 	if err != nil {
 		log.Fatalf("failed to get goimports: %v", err)
 	}
 
-	gopls, err := artifact.Gopls(context)
+	gopls, err := artifact.Gopls(ctx)
 	if err != nil {
 		log.Fatalf("failed to get gopls: %v", err)
 	}
 
-	protoc, err := artifact.Protoc(context)
+	protoc, err := artifact.Protoc(ctx)
 	if err != nil {
 		log.Fatalf("failed to get protoc: %v", err)
 	}
 
-	protocGenGo, err := artifact.ProtocGenGo(context)
+	protocGenGo, err := artifact.ProtocGenGo(ctx)
 	if err != nil {
 		log.Fatalf("failed to get protoc-gen-go: %v", err)
 	}
 
-	protocGenGoGRPC, err := artifact.ProtocGenGoGRPC(context)
+	protocGenGoGRPC, err := artifact.ProtocGenGoGRPC(ctx)
 	if err != nil {
 		log.Fatalf("failed to get protoc-gen-go-grpc: %v", err)
 	}
 
-	staticcheck, err := artifact.Staticcheck(context)
+	staticcheck, err := artifact.Staticcheck(ctx)
 	if err != nil {
 		log.Fatalf("failed to get staticcheck: %v", err)
 	}
 
-	ttyd, err := context.FetchArtifactAlias("ttyd:1.7.7")
+	ttyd, err := ctx.FetchArtifactAlias("ttyd:1.7.7")
 	if err != nil {
 		log.Fatalf("failed to get ttyd: %v", err)
 	}
 
-	vhs, err := context.FetchArtifactAlias("vhs:0.10.0")
+	vhs, err := ctx.FetchArtifactAlias("vhs:0.10.0")
 	if err != nil {
 		log.Fatalf("failed to get vhs: %v", err)
 	}
 
-	// Artifacts
-
-	goarch, err := language.GetGOARCH(contextTarget)
+	goarch, err := language.GetGOARCH(ctxTarget)
 	if err != nil {
-		log.Fatalf("failed to get GOARCH for target %s: %v", contextTarget, err)
+		log.Fatalf("failed to get GOARCH for target %s: %v", ctxTarget, err)
 	}
 
-	goos, err := language.GetGOOS(contextTarget)
+	goos, err := language.GetGOOS(ctxTarget)
 	if err != nil {
-		log.Fatalf("failed to get GOOS for target %s: %v", contextTarget, err)
+		log.Fatalf("failed to get GOOS for target %s: %v", ctxTarget, err)
 	}
 
 	_, err = artifact.
-		NewDevelopmentEnvironment("docket-shell", Systems).
+		NewDevelopmentEnvironment("docket-shell", systems).
 		WithArtifacts([]*string{
 			ffmpeg,
 			gobin,
@@ -104,12 +99,12 @@ func main() {
 			fmt.Sprintf("GOARCH=%s", *goarch),
 			fmt.Sprintf("GOOS=%s", *goos),
 		}).
-		Build(context)
+		Build(ctx)
 	if err != nil {
 		log.Fatalf("error building project environment: %v", err)
 	}
 
-	_, err = language.NewGo("docket", Systems).
+	_, err = language.NewGo("docket", systems).
 		WithBuildDirectory("cmd/docket").
 		WithIncludes([]string{
 			"cmd/docket",
@@ -117,12 +112,10 @@ func main() {
 			"go.mod",
 			"go.sum",
 		}).
-		Build(context)
+		Build(ctx)
 	if err != nil {
 		log.Fatalf("error building: %v", err)
 	}
 
-	// Run the build
-
-	context.Run()
+	ctx.Run()
 }
